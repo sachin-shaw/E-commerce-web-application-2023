f35734ed9fc9e7c6af512d23221e64e8a04ce3d4
sachin-shaw <96615888+sachin-shaw@users.noreply.github.com>
2023-05-15 21:45:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from 'path';
+import path from "path";
 
 //configure env
 dotenv.config();
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -30,7 +30,7 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.use('*', function(req, res){
+app.use("*", function(req, res){
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
